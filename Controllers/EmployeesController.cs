a3aa1ca2c2b0a96025d02c6aeda48bee4bf660d1
efsa <113553752+efsacode@users.noreply.github.com>
2023-11-29 23:42:08 +0300

    cleaning the code

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -19,7 +19,7 @@ namespace Application1.Controllers
             _context = context;
         }
 
-        // GET: Employees
+        
         public async Task<IActionResult> Index()
         {
               return _context.Employee != null ? 
@@ -27,7 +27,6 @@ namespace Application1.Controllers
                           Problem("Entity set 'Application1Context.Employee'  is null.");
         }
 
-        // GET: Employees/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.Employee == null)
@@ -45,15 +44,13 @@ namespace Application1.Controllers
             return View(employee);
         }
 
-        // GET: Employees/Create
+       
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Employees/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+       
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,BirthDate,Image,Name,Surname,Position")] Employee employee)
@@ -67,7 +64,7 @@ namespace Application1.Controllers
             return View(employee);
         }
 
-        // GET: Employees/Edit/5
+        
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.Employee == null)
@@ -83,9 +80,7 @@ namespace Application1.Controllers
             return View(employee);
         }
 
-        // POST: Employees/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,BirthDate,Image,Name,Surname,Position")] Employee employee)
@@ -118,7 +113,7 @@ namespace Application1.Controllers
             return View(employee);
         }
 
-        // GET: Employees/Delete/5
+        
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null || _context.Employee == null)
@@ -136,7 +131,7 @@ namespace Application1.Controllers
             return View(employee);
         }
 
-        // POST: Employees/Delete/5
+        
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
